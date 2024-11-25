672dcb1c4b67d9de58c7f71f0fe76ce563296b5d
Fl00ry <f.morina@studenti.unibg.it>
2024-11-25 09:28:17 +0100

    Test 1

--- src/Biblioteca/src/main/Main.java
+++ src/Biblioteca/src/main/Main.java
@@ -1,5 +1,26 @@
 package main;
 
-public class Main {
+import java.util.Scanner;
 
+
+public class Main {
+    public static void main(String[] args) {
+        GestioneUtenti gestioneUtenti = new GestioneUtenti();
+        Scanner scanner = new Scanner(System.in);
+
+       
+        gestioneUtenti.registraUtente("admin", "admin123", "amministratore");
+        gestioneUtenti.registraUtente("utente1", "password1", "utente");
+
+        
+        System.out.println("Inserisci il tuo username:");
+        String username = scanner.nextLine();
+        System.out.println("Inserisci la tua password:");
+        String password = scanner.nextLine();
+
+        gestioneUtenti.autenticaUtente(username, password);
+        
+     
+        gestioneUtenti.listaUtenti();
+    }
 }
