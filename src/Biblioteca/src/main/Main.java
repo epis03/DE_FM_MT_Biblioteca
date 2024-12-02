3dd9d4fa28e33de2a92b06f2da0c30c64552b791
mtironi <127733032+mtironi@users.noreply.github.com>
2024-12-02 19:22:43 +0100

    Aggiunta configurazione iniziale log4j 2

--- src/Biblioteca/src/main/Main.java
+++ src/Biblioteca/src/main/Main.java
@@ -1,9 +1,6 @@
 package main;
 
-import java.util.Scanner;
 
-
-import com.biblioteca.gestione.GestioneUtenti;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
